7cdbc23d57970a93960a8467b1b98b43e0bdafda
Nitish <nitish3658@gmail.com>
2021-12-28 17:34:12 +0530

    changed text

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">hey there</div>;
+  return <div className="App">hey nitish</div>;
 }
 
 export default App;
