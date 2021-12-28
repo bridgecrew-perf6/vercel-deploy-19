d3a1d90bfb3560c3900e4115a8d36dff98808103
Nitish <nitish3658@gmail.com>
2021-12-28 17:36:22 +0530

    changed heading

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">hey nitish</div>;
+  return <div className="App">hey nitish kumar</div>;
 }
 
 export default App;
