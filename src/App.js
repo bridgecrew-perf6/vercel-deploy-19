71bfa44ed45aa1966ec00564ef7a43cb631bbf7e
Nitish <nitish3658@gmail.com>
2021-12-29 11:14:15 +0530

    updated message

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">hi nitish kumar branch1</div>;
+  return <div className="App">hi nitish kumar prod branch1</div>;
 }
 
 export default App;
