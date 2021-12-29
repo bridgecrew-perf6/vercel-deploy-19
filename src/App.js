ceb4d8e366a997be90e984ba896dadc8dc452ae2
Nitish <nitish3658@gmail.com>
2021-12-29 21:43:18 +0530

    text changed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">hello nitish kumar master branch</div>;
+  return <div className="App">hello nitish kumar branch master</div>;
 }
 
 export default App;
